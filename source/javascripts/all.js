e3a46f110885d5ca6a41e543233c1c6efa4d44a0
zacky1972 <zacky1972@gmail.com>
2017-11-13 05:37:00 +0900

    fix #7

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,3 +3,30 @@
 var $ = require('jquery');
 require('popper.js');
 require('bootstrap');
+
+/*
+ * スムーススクロール
+ * 出典: http://www.matsuaz.com/matsumotojs/2011/04/13/1302687798504.html
+ * スクロールした後，ページ遷移するように改造した
+ */
+(function($){
+	$.fn.scrollAnchors = function(options) {
+		var settings = { duration: 500, easing: "swing" };
+		if (options) $.extend(settings,options);
+		return $(this).click(function(event) {
+			event.preventDefault();
+			var target_offset = $("a[name="+this.hash.slice(1)+"]").offset();
+			var target_top = target_offset.top;
+			$('html, body').animate({scrollTop:target_top},
+				settings.duration, settings.easing);
+			window.location.href = this.hash; // ページを遷移させる
+		});
+	}
+})(jQuery);
+
+/*
+ * scroll クラスを指定している a タグをスムーススクロールさせる設定
+ */
+$(document).ready( function() {
+	$(".scroll").scrollAnchors();
+});
